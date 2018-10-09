79f5df17ae3e91b91e06fbac6cafa520c8a2dafe
linuxdiqiu <di15218143233@outlook.com>
2018-10-09 19:58:08 +0800

    10.9/1

---
+++ python-web/p58.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding=utf-8
+#itertools
+import itertools
+
+def iter_1():
+    natual = itertools.count(1)#count
+    for n in natual:
+        print(n)
+def iter_2():
+    cs = itertools.cycle('123')#cycle
+    for n in cs:
+        print(n)
+def iter_3():
+    ns = itertools.repeat('A',2)#repeat  for会造成无限的循环
+    for n in ns:
+        print(n)
+def iter_stop():
+    natual = itertools.count(1)
+    ns = itertools.takewhile(lambda n:n<3, natual)#takewhile 利用匿名函数来控制迭代的停止
+    print(list(ns))
+def iter_chain():
+    chain = itertools.chain('abc', 'def')
+    for n in chain:
+        print(n)
+def iter_groupby():
+    groupby = itertools.groupby('AaaBbb', lambda x:x.upper())
+    for key, group in groupby:
+        print(key, tuple(group))
+
+#f1 = iter_1()
+f = iter_stop()
+#assert iter_chain()
+assert iter_groupby()
